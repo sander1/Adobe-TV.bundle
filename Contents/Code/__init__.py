09b78a10020b65d4c6351d5549dfc5bb2caaa3d1
sander1 <sanderspies+plexapp@gmail.com>
2011-03-30 04:51:56 +0200

    Code cleanup, cache times optimized

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -6,41 +6,33 @@
 
 import re
 
-###################################################################################################
-
-PLUGIN_TITLE               = 'Adobe TV'
-PLUGIN_PREFIX              = '/video/adobetv'
-BASE_URL                   = 'http://tv.adobe.com'
-
-# Default artwork and icon(s)
-PLUGIN_ARTWORK             = 'art-default.jpg'
-PLUGIN_ICON_DEFAULT        = 'icon-default.png'
+TITLE    = 'Adobe TV'
+BASE_URL = 'http://tv.adobe.com'
+ART      = 'art-default.jpg'
+ICON     = 'icon-default.png'
 
 ###################################################################################################
 
 def Start():
-  Plugin.AddPrefixHandler(PLUGIN_PREFIX, MainMenu, PLUGIN_TITLE, PLUGIN_ICON_DEFAULT, PLUGIN_ARTWORK)
-  Plugin.AddViewGroup('_List', viewMode='List', mediaType='items')
-  Plugin.AddViewGroup('_InfoList', viewMode='InfoList', mediaType='items')
+  Plugin.AddPrefixHandler('/video/adobetv', MainMenu, TITLE, ICON, ART)
+  Plugin.AddViewGroup('List', viewMode='List', mediaType='items')
+  Plugin.AddViewGroup('InfoList', viewMode='InfoList', mediaType='items')
 
-  # Set the default MediaContainer attributes
-  MediaContainer.title1    = PLUGIN_TITLE
-  MediaContainer.viewGroup = '_InfoList'
-  MediaContainer.art       = R(PLUGIN_ARTWORK)
+  MediaContainer.title1    = TITLE
+  MediaContainer.viewGroup = 'InfoList'
+  MediaContainer.art       = R(ART)
   MediaContainer.userAgent = ''
 
-  # Set the default thumb
-  DirectoryItem.thumb = R(PLUGIN_ICON_DEFAULT)
-  WebVideoItem.thumb = R(PLUGIN_ICON_DEFAULT)
+  DirectoryItem.thumb = R(ICON)
+  WebVideoItem.thumb = R(ICON)
 
-  # Set HTTP headers
   HTTP.CacheTime = CACHE_1WEEK
-  HTTP.Headers['User-agent'] = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2) Gecko/20100115 Firefox/3.6'
+  HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.16) Gecko/20110319 Firefox/3.6.16'
 
 ###################################################################################################
 
 def MainMenu():
-  dir = MediaContainer(viewGroup='_List')
+  dir = MediaContainer(viewGroup='List')
   dir.Append(Function(DirectoryItem(Products, title='Products')))
   dir.Append(Function(DirectoryItem(Channels, title='Channels')))
   return dir
@@ -49,15 +41,12 @@ def MainMenu():
 
 def Products(sender):
   dir = MediaContainer(title2=sender.itemTitle)
+  
+  for product in HTML.ElementFromURL(BASE_URL + '/products/', errors='ignore').xpath('//div[@id="products"]//li/a'):
+    url = BASE_URL + product.get('href')
 
-  url = BASE_URL + '/products/'
-  xp = '/html/body//div[@id="products"]//li/a'
-  products = HTML.ElementFromURL(url, errors='ignore').xpath(xp)
-
-  for p in products:
-    url = BASE_URL + p.get('href')
-    xp = '/html/body//div[@class="masthead"]'
-    details = HTML.ElementFromURL(url, errors='ignore').xpath(xp)[0]
+    # The ?c=l added to the url below doesn't do anything. It just makes the urls unique so we can use different cache times for the same url.
+    details = HTML.ElementFromURL(url + '?c=l', errors='ignore', cacheTime=CACHE_1MONTH).xpath('//div[@class="masthead"]')[0]
 
     title = details.xpath('./h1')[0].text.replace('Adobe','').strip()
     summary = details.xpath('./h2')[0].text
@@ -69,16 +58,19 @@ def Products(sender):
 ####################################################################################################
 
 def Channels(sender):
-  dir = MediaContainer(viewGroup='_List', title2=sender.itemTitle)
-
-  url = BASE_URL + '/channels/'
-  xp = '/html/body//div[@id="channels"]//div[@class="channel"]'
-  channels = HTML.ElementFromURL(url, errors='ignore').xpath(xp)
+  dir = MediaContainer(viewGroup='List', title2=sender.itemTitle)
   i = 0
 
-  for c in channels:
-    title = c.xpath('./h3//text()')[0].strip()
-    dir.Append(Function(DirectoryItem(SubChannels, title=title), i=i))
+  for channel in HTML.ElementFromURL(BASE_URL + '/channels/', errors='ignore').xpath('//div[@id="channels"]//div[@class="channel"]'):
+    title = channel.xpath('./h3//text()')[0].strip()
+
+    # If a channel has subchannels we get an extra menu
+    if len( channel.xpath('./ul/li/a') ) > 0:
+      dir.Append(Function(DirectoryItem(SubChannels, title=title), i=i))
+    else:
+      url = BASE_URL + channel.xpath('./h3/a')[0].get('href')
+      dir.Append(Function(DirectoryItem(Shows, title=title), url=url))
+
     i += 1
 
   return dir
@@ -86,22 +78,12 @@ def Channels(sender):
 ####################################################################################################
 
 def SubChannels(sender, i):
-  dir = MediaContainer(viewGroup='_List', title2=sender.itemTitle)
+  dir = MediaContainer(viewGroup='List', title2=sender.itemTitle)
 
-  url = BASE_URL + '/channels/'
-  xp = '/html/body//div[@id="channels"]//div[@class="channel"]'
-  channels = HTML.ElementFromURL(url, errors='ignore').xpath(xp)
-
-  # 'Main' item
-  title = channels[i].xpath('./h3//text()')[0].strip()
-  url = BASE_URL + channels[i].xpath('./h3/a')[0].get('href')
-  dir.Append(Function(DirectoryItem(Shows, title=title), url=url))
-
-  # Sub
-  subchannels = channels[i].xpath('./ul/li/a')
-  for s in subchannels:
-    title = s.text.strip()
-    url = BASE_URL + s.get('href')
+  channels = HTML.ElementFromURL(BASE_URL + '/channels/', errors='ignore').xpath('//div[@id="channels"]//div[@class="channel"]')
+  for subchannel in channels[i].xpath('./ul/li/a'):
+    title = subchannel.text.strip()
+    url = BASE_URL + subchannel.get('href')
     dir.Append(Function(DirectoryItem(Shows, title=title), url=url))
 
   return dir
@@ -111,14 +93,11 @@ def SubChannels(sender, i):
 def Shows(sender, url):
   dir = MediaContainer(title2=sender.itemTitle)
 
-  xp = '/html/body//div[contains(@class, "page all")]/div/div[@class="top"]'
-  shows = HTML.ElementFromURL(url, errors='ignore').xpath(xp)
-
-  for s in shows:
-    title = s.xpath('./img')[0].get('alt').strip()
-    summary = s.xpath('./p')[0].text
-    thumb = s.xpath('./img')[0].get('src').replace('50.jpg','100.jpg')
-    url = BASE_URL + s.xpath('./h3/a')[0].get('href')
+  for show in HTML.ElementFromURL(url, errors='ignore').xpath('//div[contains(@class, "page all")]/div/div[@class="top"]'):
+    title = show.xpath('./img')[0].get('alt').strip()
+    summary = show.xpath('./p')[0].text
+    thumb = show.xpath('./img')[0].get('src').replace('50.jpg','100.jpg')
+    url = BASE_URL + show.xpath('./h3/a')[0].get('href')
     dir.Append(Function(DirectoryItem(Episodes, title=title, summary=summary, thumb=Function(GetThumb, url=thumb)), url=url))
 
   return dir
@@ -128,17 +107,18 @@ def Shows(sender, url):
 def Episodes(sender, url):
   dir = MediaContainer(title2=sender.itemTitle)
 
-  xp = '/html/body//div[@id="episodes"]/table/tbody/tr'
-  episodes = HTML.ElementFromURL(url, errors='ignore').xpath(xp)
-
-  for e in episodes:
-    title = e.xpath('./td//span[@class="title"]')[0].text.strip()
-    summary = e.xpath('./td//span[@class="description"]')[0].text
-    duration = e.xpath('./td//span[@class="runtime"]')[0].text
-    duration = CalculateDuration(duration)
+  for episode in HTML.ElementFromURL(url, errors='ignore').xpath('//div[@id="episodes"]/table/tbody/tr'):
+    title = episode.xpath('./td//span[@class="title"]')[0].text.strip()
+    summary = episode.xpath('./td//span[@class="description"]')[0].text
 
     try:
-      rating = e.xpath('./td//span[@class="rating-stars"]/span')[0].get('style')
+      duration = episode.xpath('./td//span[@class="runtime"]')[0].text
+      duration = CalculateDuration(duration)
+    except:
+      duration = None
+
+    try:
+      rating = episode.xpath('./td//span[@class="rating-stars"]/span')[0].get('style')
       rating = re.search('width: ([0-9]+)%').group(1)
       if int(rating) > 0:
         rating = int(rating) / 10
@@ -147,14 +127,13 @@ def Episodes(sender, url):
     except:
       rating = None
 
-    date = e.xpath('./td//span[@class="added"]')[0].text
+    date = episode.xpath('./td//span[@class="added"]')[0].text
     date = re.search('Added : (.+)$', date).group(1)
 
-    url = BASE_URL + e.xpath('./td/a')[0].get('href')
+    url = BASE_URL + episode.xpath('./td/a')[0].get('href')
 
     try:
-      thumb_xp = '/html/head/link[@rel="image_src"]'
-      thumb = HTML.ElementFromURL(url, errors='ignore', cacheTime=CACHE_1MONTH).xpath(thumb_xp)[0].get('href')
+      thumb = HTML.ElementFromURL(url, errors='ignore', cacheTime=CACHE_1MONTH).xpath('//head/link[@rel="image_src"]')[0].get('href')
     except:
       thumb = None
 
@@ -176,18 +155,17 @@ def CalculateDuration(timecode):
 
 def GetThumb(url):
   try:
-    if url != None and url[0:4] == 'http':
+    if url and url[0:4] == 'http':
       data = HTTP.Request(url, cacheTime=CACHE_1MONTH).content
       return DataObject(data, 'image/jpeg')
   except:
     pass
-  return Redirect(R(PLUGIN_ICON_DEFAULT))
+  return Redirect(R(ICON))
 
 ####################################################################################################
 
 def PlayVideo(sender, url):
-  xp = '/html/head/link[@rel="video_src"]'
-  video_src = HTML.ElementFromURL(url, errors='ignore', cacheTime=CACHE_1MONTH).xpath(xp)[0].get('href')
+  video_src = HTML.ElementFromURL(url, errors='ignore', cacheTime=CACHE_1MONTH).xpath('//head/link[@rel="video_src"]')[0].get('href')
   vid = re.search('fileID=([0-9]+).+context=([0-9]+)', video_src)
   fileID = int(vid.group(1))
   context = int(vid.group(2))
@@ -195,8 +173,7 @@ def PlayVideo(sender, url):
   url = DoAmfRequest(fileID, context)
 
   if url.find('edgeboss') != -1:
-    xp = '/FLVPlayerConfig/stream/entry'
-    content = XML.ElementFromURL(url, errors='ignore', cacheTime=CACHE_1MONTH).xpath(xp)[0]
+    content = XML.ElementFromURL(url, errors='ignore', cacheTime=CACHE_1MONTH).xpath('/FLVPlayerConfig/stream/entry')[0]
     serverName = content.xpath('./serverName')[0].text
     appName = content.xpath('./appName')[0].text
     streamName = content.xpath('./streamName')[0].text
