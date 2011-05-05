93949f0478d164d2d00c9508749598dd5e379c8e
sander1 <sanderspies+plexapp@gmail.com>
2011-05-05 03:56:43 +0200

    Rating needs to be a float

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -136,7 +136,7 @@ def Episodes(title, url):
       rating = episode.xpath('./td//span[@class="rating-stars"]/span')[0].get('style')
       rating = re.search('width:([0-9]+)%', rating).group(1)
       if int(rating) > 0:
-        rating = int(rating) / 10
+        rating = float(rating) / 10
       else:
         rating = None
     except:
